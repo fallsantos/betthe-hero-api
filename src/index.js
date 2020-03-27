d988f321c0cabe93f6e8874275b294cbd825e7a8
Flávio Santos <fallsantosdev@hotmail.com>
2020-03-27 18:21:53 -0300

    :memo: dev route

--- src/index.js
+++ src/index.js
@@ -12,6 +12,25 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  return res.json(
+    {
+      name: 'Be The Hero api',
+      version: '0.1',
+      author: [
+        {
+          name: 'Flávio Santos',
+          rule: 'Fullstack developer',
+          email: 'fallsantosdev@hotmail.com',
+          github: 'https://github.com/fallsantos',
+          linkedin: 'https://www.linkedin.com/in/fall-santos-23bb87103/'
+        }
+      ]
+    }
+  )
+})
+
 app.use('/users', routes)
 
 app.listen(process.env.PORT, () => {
